89e23b2bbec02b1bad4b78bcd1840cf7709cd897
heroBring- <96670202+HeroAuuuu@users.noreply.github.com>
2024-06-23 02:26:49 +0800

    Update ModalVideo.js

--- themes/landing/components/ModalVideo.js
+++ themes/landing/components/ModalVideo.js
@@ -82,37 +82,7 @@ export default function ModalVideo({
                     />
                     {/* End: Modal backdrop */}
 
-                    {/* Modal dialog */}
-                    <Transition.Child
-                        className="fixed inset-0 z-[99999] overflow-hidden flex items-center justify-center transform px-4 sm:px-6"
-                        enter="transition ease-out duration-200"
-                        enterFrom="opacity-0 scale-95"
-                        enterTo="opacity-100 scale-100"
-                        leave="ttransition ease-out duration-200"
-                        leaveFrom="oopacity-100 scale-100"
-                        leaveTo="opacity-0 scale-95"
-                    >
-                        <div className="max-w-6xl mx-auto h-full flex items-center">
-                            <Dialog.Panel className="w-full max-h-full aspect-video bg-black overflow-hidden">
-                                {/* <video ref={videoRef} width={videoWidth} height={videoHeight} loop controls>
-                                <source src={video} type="video/mp4" />
-                                Your browser does not support the video tag.
-                                </video> */}
-                                <div>
-                                    <iframe
-                                        className="video-iframe aspect-video w-screen md:w-[800px] mx-auto"
-                                        src={siteConfig('LANDING_HERO_VIDEO_IFRAME', null, CONFIG)}
-                                        scrolling="no"
-                                        border="0"
-                                        frameBorder="no"
-                                        allowfullscreen="true"
-                                    ></iframe>
-                                </div>
 
-                            </Dialog.Panel>
-                        </div>
-                    </Transition.Child>
-                    {/* End: Modal dialog */}
 
                 </Dialog>
             </Transition>
