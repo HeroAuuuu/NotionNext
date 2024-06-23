64a0d99140c5f1362dd40b66ec218e5726195c86
heroBring- <96670202+HeroAuuuu@users.noreply.github.com>
2024-06-23 19:16:47 +0800

    Update Features.js

--- themes/landing/components/Features.js
+++ themes/landing/components/Features.js
@@ -22,7 +22,7 @@ export default function Features() {
   }, [])
 
   return (
-    <section className="relative">
+    <section className="relative" id="main">
 
       {/* Section background (needs .relative class on parent and next sibling elements) */}
       <div className="absolute inset-0 bg-gray-100 dark:bg-black pointer-events-none mb-16" aria-hidden="true"></div>
