c9c7a9d9a25d90481493b9e38eb11d2dc7aa171c
heroBring- <96670202+HeroAuuuu@users.noreply.github.com>
2024-06-23 14:49:36 +0800

    Update Features.js

--- themes/landing/components/Features.js
+++ themes/landing/components/Features.js
@@ -131,7 +131,7 @@ export default function Features() {
                     unmount={false}
                   >
                     <div className="relative inline-flex flex-col">
-                        <LazyImage src='/images/feature2.webp'/>
+                        <LazyImage src='/images/feature3.webp'/>
                     </div>
                   </Transition>
                   {/* Item 3 */}
@@ -149,7 +149,7 @@ export default function Features() {
                     unmount={false}
                   >
                     <div className="relative inline-flex flex-col">
-                        <LazyImage src='/images/feature3.webp'/>
+                        <LazyImage src='/images/feature2.webp'/>
                     </div>
                   </Transition>
                 </div>
